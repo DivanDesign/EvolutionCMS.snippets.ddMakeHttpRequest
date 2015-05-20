2176dd2fc0cc8d505ca5b9413f1c91fb11e74e5a
Ilyas Ronef <ilyas@divandesign.biz>
2015-05-20 10:54:43 +0500

    Version 1.1: + Добавлен метод запроса get. + Добавлена проверка на существование параметров для отправки, если параметры отсутсвуют, то запрос происходит методом get. + Добавлена возможность передавать параметры для отправки объектом.

--- ddMakeHttpRequest.php
+++ ddMakeHttpRequest.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * ddMakeHttpRequest.php
- * @version 1.0 (2011-06-16)
+ * @version 1.1 (2011-09-28)
  * 
- * @desc Осуществляет POST запрос по заданному URL.
+ * @desc Осуществляет запрос по заданному URL.
  * 
  * @param $url - Адрес, к которому обращаться.
+ * @param $metod - Тип запроса (get или post, get по умолчанию).
  * @param $post - Переменные, которые нужно отправить. Пары ключ-значение разделённые '||', разделитель между ключом и значением — '::'.
  * @param $ssl - Соединяемся ли с https.
  * @param $headers - Заголовки, которые нужно отправить. Разделитель между строками — '||'.
@@ -16,7 +17,8 @@
  */
 
 if (isset($url)){
-	$post = isset($post) ? explode('||', $post) : false;
+	if (!is_array($post)) $post = isset($post) ? $post : false;
+	$metod = ((isset($metod) && $mettod == 'post') || is_array($post)) ? 'post' : 'get';
 	$ssl = (isset($ssl) && ($ssl == '1')) ? true : false;
 	$headers = isset($headers) ? explode('||', $headers) : false;
 
@@ -31,22 +33,36 @@ if (isset($url)){
 
 	//Результат должен быть возвращен, а не выведен
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	//Запрос будет методом POST типа application/x-www-form-urlencoded (используемый браузерами при отправке форм)
-	curl_setopt($ch, CURLOPT_POST, 1);
 	//Не включаем полученные заголовки в результат
 	curl_setopt($ch, CURLOPT_HEADER, 0);
-	//При установке этого параметра в ненулевое значение, при получении HTTP заголовка "Location: " будет происходить перенаправление на указанный этим заголовком URL (это действие выполняется рекурсивно, для каждого полученного заголовка "Location:").
-	// 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	//При установке этого параметра в ненулевое значение, при получении HTTP заголовка "Location: " будет происходить перенаправление
+	//на указанный этим заголовком URL (это действие выполняется рекурсивно, для каждого полученного заголовка "Location:").
+	//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 	//Если есть переменные для отправки
-	if (is_array($post)){
-		$_post = Array();
-		//Сформируем массив для отправки, предварительно перекодировав
-		foreach ($post as $value){
-			$value = explode('::', $value);
-			$_post[] = $value[0].'='.urlencode($value[1]);
+	if ($metod == 'post' && isset($post)){
+		//Запрос будет методом POST типа application/x-www-form-urlencoded (используемый браузерами при отправке форм)
+		curl_setopt($ch, CURLOPT_POST, 1);
+
+		//Если пост передан строкой, то преобразовываем в массив
+		if (!is_array($post)){
+			$post = explode('||', $post);
+			$temp = $post;
+			$post = array();
+			foreach ($temp as $value){
+				$value = explode('::', $value);
+				$post[$value[0]] = $value[1];
+			}
 		}
-		curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $_post));
+
+		if (is_array($post)){
+			$post_mas = Array();
+			//Сформируем массив для отправки, предварительно перекодировав
+			foreach ($post as $key=>$value){
+				$post_mas[] = $key.'='.urlencode($value);
+			}
+			curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $post_mas));
+		}
 	}
 
 	//Если заданы какие-то HTTP заголовки
