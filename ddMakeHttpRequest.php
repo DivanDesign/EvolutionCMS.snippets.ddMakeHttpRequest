fc69b8dde9ec36f71f0e3c855b613218b9f9fafb
Ilyas Ronef <ilyas@divandesign.biz>
2015-05-20 11:14:26 +0500

    Version 1.1.2: + Добавлена ручная обработка переадресации, в случае невозможности использования автоматической (CURLOPT_FOLLOWLOCATION).

--- ddMakeHttpRequest.php
+++ ddMakeHttpRequest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * ddMakeHttpRequest.php
- * @version 1.1.1 (2013-09-13)
+ * @version 1.1.2 (2013-10-22)
  * 
  * @desc Осуществляет запрос по заданному URL.
  * 
@@ -17,10 +17,13 @@
  */
 
 if (isset($url)){
-	if (!is_array($post)) $post = isset($post) ? $post : false;
-	$metod = ((isset($metod) && $mettod == 'post') || is_array($post)) ? 'post' : 'get';
+	if (!is_array($post))
+		$post = isset($post) ? $post : false;
+	$metod = ((isset($metod) && $metod == 'post') || is_array($post)) ? 'post' : 'get';
 	$ssl = (isset($ssl) && ($ssl == '1')) ? true : false;
 	$headers = isset($headers) ? explode('||', $headers) : false;
+
+	$manualRedirect = false;
 
 	//Инициализируем сеанс CURL
 	$ch = curl_init($url);
@@ -34,10 +37,16 @@ if (isset($url)){
 	//Результат должен быть возвращен, а не выведен
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	//Не включаем полученные заголовки в результат
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	//При установке этого параметра в ненулевое значение, при получении HTTP заголовка "Location: " будет происходить перенаправление
-	//на указанный этим заголовком URL (это действие выполняется рекурсивно, для каждого полученного заголовка "Location:").
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+	if (ini_get('open_basedir') != '' || ini_get('safe_mode')){
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		$manualRedirect = true;
+	}else{
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		//При установке этого параметра в ненулевое значение, при получении HTTP заголовка "Location: " будет происходить перенаправление
+		//на указанный этим заголовком URL (это действие выполняется рекурсивно, для каждого полученного заголовка "Location:").
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	}
 	curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
 
 	//Если есть переменные для отправки
@@ -59,7 +68,7 @@ if (isset($url)){
 		if (is_array($post)){
 			$post_mas = Array();
 			//Сформируем массив для отправки, предварительно перекодировав
-			foreach ($post as $key=>$value){
+			foreach ($post as $key => $value){
 				$post_mas[] = $key.'='.urlencode($value);
 			}
 			curl_setopt($ch, CURLOPT_POSTFIELDS, implode('&', $post_mas));
@@ -67,16 +76,71 @@ if (isset($url)){
 	}
 
 	//Если заданы какие-то HTTP заголовки
-	if (is_array($headers)){curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);}
+	if (is_array($headers)){
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	}
 
 	//Если задан UserAgent
-	if (isset($uagent)){curl_setopt($ch, CURLOPT_USERAGENT, $uagent);}
+	if (isset($uagent)){
+		curl_setopt($ch, CURLOPT_USERAGENT, $uagent);
+	}
 
 	//Выполняем запрос
 	$result = curl_exec($ch);
 
 	//Если есть ошибки или ничего не получили
-	if (curl_errno($ch) != 0 && empty($result)){$result = false;}
+	if (curl_errno($ch) != 0 && empty($result)){
+		$result = false;
+	}else if ($manualRedirect){
+		$redirectCount = 10;
+		while (0 < $redirectCount--){
+			//Получаем заголовки, контент и код ответа
+			$resultHeader = substr($result, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+			$resultData = substr($result, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+			$resultResponseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+			//Проверяем код на редирект
+			if (intval($resultResponseCode / 100) == 3){
+				//Ищем новый url в заголовках
+				$matches = array();
+				preg_match('/Location:(.*?)\n/', $resultHeader, $matches);
+				$newUrlStr = '';
+				if (count($matches)){
+					$newUrlStr = array_pop($matches);
+				}
+
+				//Парсим url
+				$redirectUrl = parse_url(trim($newUrlStr));
+				if (!is_array($redirectUrl)){
+					$redirectUrl = array();
+				}
+
+				//Собираем новый url
+				$lastUrl = parse_url(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+				if (!$redirectUrl['scheme']){
+					$redirectUrl['scheme'] = $lastUrl['scheme'];
+				}
+				if (!$redirectUrl['host']){
+					$redirectUrl['host'] = $lastUrl['host'];
+				}
+				if (!$redirectUrl['path']){
+					$redirectUrl['path'] = $lastUrl['path'];
+				}
+				$newUrl = $redirectUrl['scheme'].'://'.$redirectUrl['host'].$redirectUrl['path'].($redirectUrl['query'] ? '?'.$redirectUrl['query'] : '');
+
+				//Выполняем запрос с новым адресом
+				curl_setopt($ch, CURLOPT_URL, $newUrl);
+				$result = curl_exec($ch);
+				if (curl_errno($ch) != 0 && empty($result)){
+					$result = false;
+					break;
+				}
+			}else{
+				$result = $resultData;
+				break;
+			}
+		}
+	}
 
 	//Закрываем сеанс CURL
 	curl_close($ch);
